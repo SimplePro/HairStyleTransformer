a0390c7d0d1ae8518e83cec2a5720e12c146a147
kdhsimplepro <kdhsimplepro@gmail.com>
2023-05-08 00:11:42 +0900

    modified some errors

--- SSL/utils.py
+++ SSL/utils.py
@@ -9,6 +9,8 @@ from tqdm import tqdm
 
 import torch
 from torch.utils.data import Dataset
+from torchvision import transforms
+import torchvision.transforms.functional as F
 
 import os
 
@@ -32,23 +34,23 @@ class SSLDataset(Dataset):
 
         # class별로 데이터의 개수를 확인하고, 가장 작은 값을 대입
         # -> 데이터 편향을 아예 없애기 위함임.
-        self.data_len_per_class = min([len(os.listdir(os.path.join(self.dir, cls))) for cls in self.classes])
+        self.data_len_per_class = min([len(os.listdir(os.path.join(self.dir_, cls))) for cls in self.classes])
 
         # class를 돌면서
         for cls in self.classes:
-            paths = glob(os.path.join(self.dir, cls)) # 각 클래스의 이미지 경로들을
+            paths = glob(os.path.join(self.dir_, cls, "*.jpg")) # 각 클래스의 이미지 경로들을
             shuffle(paths) # 섞어줌
 
             for i in range(len(paths)):
 
                 if i < self.data_len_per_class: # 편향되지 않게 데이터 개수를 고려하여 selected_data 변수에 추가하고
-                    self.selected_data.append((paths[i], cls))
+                    self.selected_data.append((paths[i], int(cls)))
 
                 else: # 그 외는 사용하지 않는 데이터 변수에 추가함
-                    self.unselected_data.append((paths[i], cls))
+                    self.unselected_data.append((paths[i], int(cls)))
 
     def __len__(self):
-        return len(self.selected_paths)
+        return len(self.selected_data)
         
     def __getitem__(self, index):
         path, y = self.selected_data[index]
@@ -60,21 +62,26 @@ class SSLDataset(Dataset):
 
 
 if __name__ == '__main__':
-    # label_frame.csv 읽기
-    label_frames = pd.read_csv("../dataset/label_frame.csv")
-    label_array = label_frames.to_numpy().tolist()
 
-    # hair_img를 label에 따라 분류하여 ssl_dataset에 추가
-    for file, forehead, length in tqdm(label_array):
-        # hair_img 읽기
-        hair_img = Image.open(f"../dataset/hair/{file}")
+    ## ssl_dataset 추가
+    # # label_frame.csv 읽기
+    # label_frames = pd.read_csv("../dataset/label_frame.csv")
+    # label_array = label_frames.to_numpy().tolist()
 
-        # labeled data라면
-        if forehead in ["0", "1", "2"]:
-            hair_img.save(f"ssl_dataset/forehead/labeled/{forehead}/{file}") # 저장
+    # # hair_img를 label에 따라 분류하여 ssl_dataset에 추가
+    # for file, forehead, length in tqdm(label_array):
+    #     # hair_img 읽기
+    #     hair_img = Image.open(f"../dataset/hair/{file}")
 
-        # unlabeled data라면
-        elif type(forehead) == float and np.isnan(forehead):
-            hair_img.save(f"ssl_dataset/forehead/unlabeled/{file}")
+    #     # labeled data라면
+    #     if forehead in ["0", "1", "2"]:
+    #         hair_img.save(f"ssl_dataset/forehead/labeled/{forehead}/{file}") # 저장
 
-        # label이 -1인 datasample은 아예 사용하지 않음
+    #     # unlabeled data라면
+    #     elif type(forehead) == float and np.isnan(forehead):
+    #         hair_img.save(f"ssl_dataset/forehead/unlabeled/{file}")
+
+    #     # label이 -1인 datasample은 아예 사용하지 않음
+
+    ssl_dataset = SSLDataset(dir_="./ssl_dataset/forehead/labeled/", transform_=transforms.Compose([]))
+    print(ssl_dataset[-1])
